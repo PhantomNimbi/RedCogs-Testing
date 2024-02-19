cb13d0e2144daf590f44aad85ffad478f359f830
Nimbi <JoshuaVL1988@outlook.com>
2024-02-18 20:15:22 -0700

    ...

--- logger/logger.py
+++ logger/logger.py
@@ -41,42 +41,42 @@ class Logger(commands.Cog):
         await ctx.send('Successfully enabled logger') 
         
         
-        @commands.Cog.listener()
-        async def on_guild_join(self, ctx, guild):
-            """
-            Log guild join events to the logger channel if it is set
-            """
+    @commands.Cog.listener()
+    async def on_guild_join(self, ctx, guild):
+        """
+        Log guild join events to the logger channel if it is set
+        """
             
-            if (self.config.logger_channel == None):                
-                return
-            else:
-                logger_channel = await self.bot.get_channel(self.config.logger_channel)
+        if (self.config.logger_channel == None):                
+            return
+        else:
+            logger_channel = await self.bot.get_channel(await self.config.logger_channel())
             
-                e = discord.Embed(title='Logger', description='{} has entered a new guild.'.format(ctx.bot.name), timestamp=datetime.datetime.utcnow())
-                e.add_field(name='Guild Name', value='{}'.format(box[guild.name]), inline=True),
-                e.add_field(name='Guild ID', value='{}'.format(box[guild.id]), inline=True)
-                e.color(discord.Color.blue)
-                e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(self.bot.user.display_avatar.url))
-                await logger_channel.send(embed=e)
-            
-            
-        @commands.Cog.listener()
-        async def on_error(self, ctx, guild, error):
-            """
-            Log on error events to the logger channel if it is set
-            """
-            
-            if (self.config.logger_channel == None):                
-                return
-            else:
-                logger_channel = await self.bot.get_channel(self.config.logger_channel)
+            e = discord.Embed(title='Logger', description='{} has entered a new guild.'.format(ctx.bot.name), timestamp=datetime.datetime.utcnow())
+            e.add_field(name='Guild Name', value='{}'.format(box[guild.name]), inline=True),
+            e.add_field(name='Guild ID', value='{}'.format(box[guild.id]), inline=True)
+            e.color(discord.Color.blue)
+            e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(self.bot.user.display_avatar.url))
+            await logger_channel.send(embed=e)
             
             
-                e = discord.Embed(title='Logger', description='{} has encountered an error!'.format(ctx.bot.name), timestamp=datetime.datetime.utcnow())
-                e.add_field(name='Guild Name', value='{}'.format(box[guild.name]), inline=True),
-                e.add_field(name='Guild ID', value='{}'.format(box[guild.id]), inline=True)
-                e.add_field(name='\u200b', value='\u200b')
-                e.add_field(name='Error Stack', value='{}'.format(warning[error.stack]), inline=False)
-                e.color(discord.Color.blue)
-                e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(self.bot.user.display_avatar.url))
-                await logger_channel.send(embed=e)
+    @commands.Cog.listener()
+    async def on_error(self, ctx, guild, error):
+        """
+        Log on error events to the logger channel if it is set
+        """
+            
+        if (self.config.logger_channel == None):                
+            return
+        else:
+            logger_channel = await self.bot.get_channel(await self.config.logger_channel())
+            
+            
+            e = discord.Embed(title='Logger', description='{} has encountered an error!'.format(ctx.bot.name), timestamp=datetime.datetime.utcnow())
+            e.add_field(name='Guild Name', value='{}'.format(box[guild.name]), inline=True),
+            e.add_field(name='Guild ID', value='{}'.format(box[guild.id]), inline=True)
+            e.add_field(name='\u200b', value='\u200b')
+            e.add_field(name='Error Stack', value='{}'.format(warning[error.stack]), inline=False)
+            e.color(discord.Color.blue)
+            e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(self.bot.user.display_avatar.url))
+            await logger_channel.send(embed=e)
