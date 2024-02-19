6530633f40b0b307636fc2f4ca19c8d28f3773e1
Nimbi <JoshuaVL1988@outlook.com>
2024-02-18 21:01:41 -0700

    ...

--- logger/logger.py
+++ logger/logger.py
@@ -59,29 +59,4 @@ class Logger(commands.Cog):
             e.set_footer(text='Powered by Red-DiscordBot', icon_url=self.bot.user.display_avatar.url)
             await logger_channel.send(embed=e)
             
-    
-    
-    
-    # Global error handler for future update
-    # Still working on the full scope of what types of errors to build this for
-    #         
-    # @commands.Cog.listener()
-    # async def on_command_error(self, guild):
-    #     """
-    #     Log on error events to the logger channel if it is set
-    #     """
-    #         
-    #     if (self.config.logger_channel == None):                
-    #         return
-    #     else:
-    #         logger_channel = self.bot.get_channel(await self.config.logger_channel())
-    #         
-    #         
-    #         e = discord.Embed(title='Logger', description='{} has encountered an error!'.format(self.bot.name), timestamp=datetime.datetime.utcnow())
-    #         e.add_field(name='Guild Name', value='{}'.format(box[self.bot.guild.name]), inline=True),
-    #         e.add_field(name='Guild ID', value='{}'.format(box[self.bot.guild.id]), inline=True)
-    #         e.add_field(name='\u200b', value='\u200b')
-    #         e.add_field(name='Error Stack', value='{}'.format(warning['{}'.format(error)]), inline=False)
-    #         e.color(discord.Color.blue)
-    #         e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(self.bot.user.display_avatar.url))
-    #         await logger_channel.send(embed=e)
+            
