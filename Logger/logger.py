a1483e341928100dd992c6df9d92b3ff6a24666f
Nimbi <JoshuaVL1988@outlook.com>
2024-02-18 18:24:31 -0700

    Update Logger Cog

--- Logger/logger.py
+++ Logger/logger.py
@@ -3,6 +3,7 @@ from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box, warning
 from discord import Embed, TextChannel
+from datetime import datetime
 
 class Logger(commands.Cog):
     """_summary_
@@ -15,11 +16,11 @@ class Logger(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self)
         
-        logger_config = {
-            # ...   
-        }
-        
-        self.config.register_global(**logger_config)
+        self.config.register_global(
+            channel=True,
+            options=True,
+            guild=True
+        )
         
     
     @commands.group()
@@ -41,11 +42,11 @@ class Logger(commands.Cog):
             option (Literal[&quot;guilds&quot;, &quot;errors&quot;]): _description_
             channel (TextChannel): _description_
         """
-        ch = await self.config.set(channel)
-        opt = await self.config.set(option)
+        c = await self.config.channel.set(channel)
+        o = await self.config.options.set(option)
         
-        logger_channel = await Config.channel(ch)
-        logger_options = await Config.option(opt)
+        logger_channel = await self.config.channel(c)
+        logger_options = await self.config.options(o)
         
         if (option == 'guilds'):
             option_choice = 'Guild Join'
@@ -54,9 +55,24 @@ class Logger(commands.Cog):
                 option_choice = 'Errors'
                 
         
-        e = Embed(title='Logger', description='Logger has been enabled')
+        e = Embed(title='Logger', description='Logger has been enabled', timestamp=datetime.utcnow())
         e.add_field(name='{}'.format(option_choice), value='{}'.format(logger_options), inline=True)        
         e.add_field(name='Logging Channel', value='{}'.format(logger_channel), inline=True)
         e.color('Blue')
+        e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(ctx.bot.getAvatarUrl()))
                 
         await ctx.send(embed=e) 
+        
+        
+        @Red.on_guild_join()
+        async def on_guild_join(self, ctx):
+            
+            ch = await Config.channel(channel)
+            logger_channel = await Config.channel(ch)
+            
+            e = Embed(title='Logger', description='{} has entered a new guild.'.format(ctx.bot.name), timestamp=datetime.utcnow())
+            e.add_field(name='Guild Name', value='{}'.format(ctx.guild.name), inline=True),
+            e.add_field(name='Guild ID', value='{}'.format(ctx.guild.id), inline=True)
+            e.color('Blue')
+            e.set_footer(text='Powered by Red-DiscordBot', icon_url='{}'.format(ctx.bot.getAvatarUrl()))
+            await logger_channel.send(embed=e)
